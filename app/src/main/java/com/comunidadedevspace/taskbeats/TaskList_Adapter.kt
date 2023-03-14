3fdab2832965b52f9982870f6e1abad167b50dff
Bruno Andrade <119361832+Andradepb@users.noreply.github.com>
2023-03-13 21:51:45 -0300

    Update TaskList_Adapter.kt

--- app/src/main/java/com/comunidadedevspace/taskbeats/TaskList_Adapter.kt
+++ app/src/main/java/com/comunidadedevspace/taskbeats/TaskList_Adapter.kt
@@ -6,12 +6,27 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class Recycle_Adapter (){
+class TaskList_Adapter (private val Title:Array<String>):
+    RecyclerView.Adapter<TaskListViewHolder>()
+{
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Recycle_Adapter.> {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: TaskListViewHolder, position: Int) {
+        val item = Title[position]
+    }
+
+    override fun getItemCount(): Int {
+        return Title.size
+    }
 
 }
 
-class ViewHolder (view: View) : RecyclerView.ViewHolder(view){
-    val
+class TaskListViewHolder (view: View) : RecyclerView.ViewHolder(view){
+
+    val Task_Title = view.findViewById<TextView>(R.id.TaskTitle)
+    val Task_Description = view.findViewById<TextView>(R.id.TaskDescription)
 
 
 }
