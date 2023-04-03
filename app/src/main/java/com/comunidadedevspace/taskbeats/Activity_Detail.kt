c75aa9cad2a88b43128e7b76aea3b5391312cc69
Bruno Andrade <119361832+Andradepb@users.noreply.github.com>
2023-04-03 16:06:44 -0300

    add menu

--- app/src/main/java/com/comunidadedevspace/taskbeats/Activity_Detail.kt
+++ app/src/main/java/com/comunidadedevspace/taskbeats/Activity_Detail.kt
@@ -2,10 +2,11 @@ package com.comunidadedevspace.taskbeats
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
 import android.widget.TextView
 
 class Activity_Detail : AppCompatActivity() {
-
     companion object{
         val TASK_DETAIL_EXTRA = "task.title.extra.detail"
     }
@@ -15,9 +16,19 @@ class Activity_Detail : AppCompatActivity() {
 
         // Recuperar textView
         val tvtitle = findViewById<TextView>(R.id.tv_TaskTitle_Detail)
+
         // Recuperar string da nova página
         val title:String = intent.getStringExtra(TASK_DETAIL_EXTRA)?: ""
+
         // Setar a nova página na tela
         tvtitle.text = title
     }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu_task_detail, menu)
+        return true
+    }
+
+
 }
