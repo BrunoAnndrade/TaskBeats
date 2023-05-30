1d9c237af5d2785dec6f5fbc0e60fdc399f3cbdc
Bruno Andrade <119361832+BrunoAnndrade@users.noreply.github.com>
2023-05-30 15:08:31 -0300

    add fragment

--- app/src/main/java/com/comunidadedevspace/taskbeats/presentation/NewsListFragment.kt
+++ app/src/main/java/com/comunidadedevspace/taskbeats/presentation/NewsListFragment.kt
@@ -7,29 +7,32 @@ import android.view.View
 import android.view.ViewGroup
 import com.comunidadedevspace.taskbeats.R
 
+
 /**
  * A simple [Fragment] subclass.
- * Use the [TaskListFragment.newInstance] factory method to
+ * Use the [NewsListFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class TaskListFragment : Fragment() {
+class NewsListFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_task_list, container, false)
+        return inflater.inflate(R.layout.fragment_news_list, container, false)
     }
 
     companion object {
         /**
          * Use this factory method to create a new instance of
          * this fragment using the provided parameters.
-         * @return A new instance of fragment TaskListFragment.
+         *
+         * @return A new instance of fragment NewsListFragment.
          */
 
         @JvmStatic
-        fun newInstance() = TaskListFragment()
+        fun newInstance() = NewsListFragment()
+
     }
 }
