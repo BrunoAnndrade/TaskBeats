9a3bffaa44a78821662815b532916325494278bc
Bruno Andrade <119361832+BrunoAnndrade@users.noreply.github.com>
2023-07-05 15:51:51 -0300

    trocar nome de fucção

--- app/src/main/java/com/comunidadedevspace/taskbeats/TaskBeatsApplication.kt
+++ app/src/main/java/com/comunidadedevspace/taskbeats/TaskBeatsApplication.kt
@@ -7,22 +7,19 @@ import com.comunidadedevspace.taskbeats.data.AppDataBase
 
 /*manter uma única instância do banco de dados durante toda
 a execução da aplicação e fornecer acesso a ele de maneira centralizada.*/
-class TaskBeatsApplication: Application() {
+class TaskBeatsApplication : Application() {
 
-    private lateinit var dataBase:AppDataBase
-
+    private lateinit var dataBase: AppDataBase
     override fun onCreate() {
         super.onCreate()
 
         //biblioteca api
         dataBase = Room.databaseBuilder(
-                applicationContext,
-                AppDataBase::class.java, "TaskBeats-DataBase"
+            applicationContext,
+            AppDataBase::class.java, "TaskBeats-DataBase"
         ).build()
     }
-
-    fun getAppDataBase():AppDataBase{
+    fun getAppDataBase(): AppDataBase {
         return dataBase
     }
-
 }
