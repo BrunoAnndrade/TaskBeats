de8e4a3d0e6653b1f7a43af38f87c349854f1a8a
Bruno Andrade <119361832+BrunoAnndrade@users.noreply.github.com>
2023-10-18 11:15:44 -0300

    edit retrofit

--- app/src/main/java/com/comunidadedevspace/taskbeats/data/remote/RetrofitModule.kt
+++ app/src/main/java/com/comunidadedevspace/taskbeats/data/remote/RetrofitModule.kt
@@ -2,6 +2,8 @@ package com.comunidadedevspace.taskbeats.data.remote
 
 import com.comunidadedevspace.taskbeats.BuildConfig
 import com.google.gson.Gson
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.create
@@ -10,7 +12,14 @@ object RetrofitModule {
 
     fun createNewsService(): NewsService {
 
-        val apiKey = BuildConfig.API_KEY
+        val logging = HttpLoggingInterceptor()
+        logging.apply {
+            HttpLoggingInterceptor.Level.BODY
+        }
+
+        val client: OkHttpClient = OkHttpClient.Builder()
+            .addInterceptor(logging)
+            .build()
 
         val retrofit = Retrofit
             .Builder()
